a45522e165a4f4ceb5285688bd2c6e432166cc05
Johnny Holton <johnny@holton.io>
2024-03-12 21:40:12 -0400

    Add functionality to derive public and private keys from an extended key
    Only supports testnet extended keys for now.

--- app/ui/bitcoin/extended-key.tsx
+++ app/ui/bitcoin/extended-key.tsx
@@ -5,12 +5,22 @@ import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 import { useDebouncedCallback } from 'use-debounce';
 import { useState } from "react";
 import Table from '@/app/ui/bitcoin/table';
+import bs58check from 'bs58check';
+import bs58 from 'bs58';
+import secp256k1 from 'secp256k1';
+
+import {
+  createHmac,
+  createHash
+} from 'crypto';
+import { Key } from '@/app/lib/definitions';
 
 export default function ExtendedKey() {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
   const [error, setError] = useState('');
+  const [keys, setKeys] = useState<Key[]>([]);
 
 
   const handleSearch = useDebouncedCallback((key) => {
@@ -19,6 +29,7 @@ export default function ExtendedKey() {
       if (key.length == 111) {
         params.set('key', key);
         setError("");
+        populate(key)
       } else {
         setError("Invalid key length. Must be 111 characters.");
       }
@@ -28,8 +39,98 @@ export default function ExtendedKey() {
     replace(`${pathname}?${params.toString()}`);
   }, 300);
 
+  function populate(xprv: string) {
+    const rawXprv = bs58check.decode(xprv)
+    const deserializedKey = deserializeKey(rawXprv)
+    const chaincode = Buffer.from(deserializedKey["chainCode"], 'hex')
+    let key = Buffer.from(deserializedKey["keyData"], 'hex')
+    if (key[0] == 0) {
+      key = key.slice(1)
+    }
+    const privs = getWalletPrivs(key, chaincode)
+
+    let pubs : Key[] = [];
+    privs.forEach(priv => {
+      let hexPriv = Buffer.from(priv).toString('hex')
+      hexPriv = `ef${hexPriv}01`
+      const hash1 = createHash('sha256').update(Buffer.from(hexPriv, 'hex')).digest();
+      const hash2 = createHash('sha256').update(hash1).digest();
+      const checksum = hash2.slice(0, 4).toString('hex');
+      hexPriv = `${hexPriv}${checksum}`;
+      const wif = bs58.encode(Buffer.from(hexPriv, 'hex'));
+      const pub = getPubFromPriv(priv);
+      pubs.push({
+        "pub": Buffer.from(pub).toString('hex'),
+        "wif": wif
+      });
+      setKeys(pubs);
+    })
+
+
+
+  }
+  const range = (n: number) => Array.from(Array(n).keys())
+
+  function getWalletPrivs(key: Uint8Array, chaincode: Uint8Array, path: [number, boolean][] = [[84, true], [1, true], [0, true], [0, false]]) {
+    let walletPrivs:Uint8Array[] = [];
+    let currentKey = key;
+    path.forEach(([index, hardened]) => {
+      const result = derivePrivChild(currentKey, chaincode, index, hardened);
+      currentKey = result.key;
+      chaincode = result.chaincode;
+    });
+
+    range(30).map(i => {
+      const result = derivePrivChild(currentKey, chaincode, i, false);
+      walletPrivs.push(result.key);
+    })
+    return walletPrivs;
+  }
+
+  function derivePrivChild(key: Uint8Array, chaincode: Uint8Array, index: number, hardened: boolean) {
+    let data;
+    if (hardened) {
+      index += 0x80000000;
+      data = Buffer.concat([Buffer.from([0]), Buffer.from(key), Buffer.from(index.toString(16), 'hex')]);
+    } else {
+      index += 0x00000000;
+      const pub = getPubFromPriv(key);
+      let indexBuff = Buffer.alloc(4);
+      indexBuff.writeUInt32BE(index)
+      data = Buffer.concat([pub, indexBuff]);
+    }
+    const hmac = createHmac('sha512', chaincode).update(data).digest();
+    const secp256k1Order = BigInt('115792089237316195423570985008687907852837564279074904382605163141518161494337')
+    const leftBytes = hmac.subarray(0, 32)
+    const leftInt = BigInt('0x' + Buffer.from(leftBytes).toString('hex'));
+    const newChaincode = hmac.subarray(32);
+
+    const newKey = (BigInt(`0x${Buffer.from(key).toString('hex')}`) + leftInt) % secp256k1Order;
+    const newKeyBytes = Buffer.from(newKey.toString(16).padStart(64, '0'), 'hex');
+    return {"key": newKeyBytes, "key_hex": newKey.toString(16), "chaincode": newChaincode}
+  }
+
+  function getPubFromPriv(key: Uint8Array) {
+    const pubKey = secp256k1.publicKeyCreate(key)
+    return pubKey;
+  }
+
+
+  function deserializeKey(xprv: Uint8Array) {
+    const hex = Buffer.from(xprv).toString('hex');
+    return {
+        "version": hex.substring(0, 8),
+        "depth": hex.substring(8, 10),
+        "fingerprint": hex.substring(10, 18),
+        "childNmber": hex.substring(18, 26),
+        "chainCode": hex.substring(26, 90),
+        "keyData": hex.substring(90,)
+    }
+  }
+
   return (
     <fieldset className="mb4 flex-1">
+      <div className="block p-2">Enter extended key (testnet)</div>
       <div className="relative flex flex-1 flex-shrink-0">
         <label htmlFor="extendedKey" className="sr-only">
           Enter extended key
@@ -43,6 +144,7 @@ export default function ExtendedKey() {
         />
         <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
       </div>
+      <div className="block p-2">Uses m/84&apos;/1&apos;/0&apos;/0 derivation path</div>
       <div id="key-error" aria-live="polite" aria-atomic="true" className="block">
         {error.length > 0 &&
           <p className="mt-2 text-sm text-red-500">
@@ -50,7 +152,7 @@ export default function ExtendedKey() {
           </p>
         }
       </div>
-      <Table keys={[]} />
+      <Table keys={keys} />
     </fieldset>
   );
 }
--- app/ui/bitcoin/table.tsx
+++ app/ui/bitcoin/table.tsx
@@ -1,7 +1,9 @@
+import { Key } from '@/app/lib/definitions';
+
 export default function InvoicesTable({
   keys
 }: {
-  keys: Array<string>;
+  keys: Array<Key>;
 }) {
 
   return (
@@ -12,20 +14,23 @@ export default function InvoicesTable({
             <thead className="rounded-lg text-left text-sm font-normal">
               <tr>
                 <th scope="col" className="px-4 py-5 font-medium sm:pl-6">
-                  key
+                  public key
+                </th>
+                <th scope="col" className="px-4 py-5 font-medium sm:pl-6">
+                  WIF
                 </th>
               </tr>
             </thead>
             <tbody className="bg-white">
               {keys?.map((key) => (
                 <tr
-                  key={key}
+                  key={key.pub}
                   className="w-full border-b py-3 text-sm last-of-type:border-none [&:first-child>td:first-child]:rounded-tl-lg [&:first-child>td:last-child]:rounded-tr-lg [&:last-child>td:first-child]:rounded-bl-lg [&:last-child>td:last-child]:rounded-br-lg"
                 >
                   <td className="whitespace-nowrap px-3 py-3">
-                    {/* {key} */}
-                    test
+                    {key.pub}
                   </td>
+                  <td>{key.wif}</td>
                 </tr>
               ))}
             </tbody>
